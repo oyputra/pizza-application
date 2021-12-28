6ce00281727ec5280ac1cecb3f2ec7b0b7d1e9eb
Oktavian Yudistira Putra <oktavianyudistiraputra@gmail.com>
2021-12-29 04:51:10 +0700

    read list data pizza

--- app/Http/Controllers/PizzaController.php
+++ app/Http/Controllers/PizzaController.php
@@ -16,7 +16,8 @@ class PizzaController extends Controller
     public function index()
     {
         //
-        return view('pizza.index');
+        $pizzas = Pizza::get();
+        return view('pizza.index', compact('pizzas'));
     }
 
     /**
